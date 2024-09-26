f802a5a3deed50ab5d29f8ae72b4c591df139c61
SteveHallen532 <lamydarioalberto@gmail.com>
2024-09-25 23:07:44 -0300

    fix heroku build

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/assets'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
